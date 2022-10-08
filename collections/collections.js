40166b32279bea11eaaa5db06748628c638036d9
Diego Montivero <diego.montivero91@gmail.com>
2022-10-08 20:48:35 +0200

    Refactor collections logic

--- collections/collections.js
+++ collections/collections.js
@@ -14,20 +14,8 @@ promptCancel.addEventListener("click", () => {
 });
 
 //Store active
-let curIndex = null;
+let inView = "";
 let timerID = null;
-
-function updateCurIndex(e) {
-  const newIndex = e.target.parentNode;
-
-  if (curIndex != null) {
-    const oldIndex = trendingContainer.children[curIndex];
-    oldIndex.classList.remove("active");
-  }
-
-  newIndex.classList.add("active");
-  curIndex = newIndex.dataset.index;
-}
 
 //Load collection buttons
 function loadCollections(list) {
@@ -43,19 +31,17 @@ function loadCollections(list) {
     const itemRemove = document.createElement("button");
 
     itemWrapper.classList.add("tag");
-    itemWrapper.dataset.index = index;
     itemName.innerText = entry.name;
+    itemName.addEventListener("click", loadCollection);
     itemRemove.innerHTML = '<i class="fas fa-times"></i>';
 
-    if (curIndex == index) {
+    if (inView == entry.name) {
       itemWrapper.classList.add("active");
     }
 
     if (index == 0) {
-      itemName.addEventListener("click", loadLovedPhotos);
       itemWrapper.append(itemName);
     } else {
-      itemName.addEventListener("click", loadCollectionPhotos);
       itemRemove.addEventListener("click", openPrompt);
       itemWrapper.append(itemName, itemRemove);
     }
@@ -64,89 +50,53 @@ function loadCollections(list) {
   });
 }
 
-//LOVED PHOTOS
-function loadLovedPhotos(e) {
-  let index = e.target.parentNode.dataset.index;
-
-  if (index === curIndex) {
+function loadCollection(event) {
+  if (event.target.parentNode.classList.contains("active")) {
     return;
+  } else {
+    const active = document.querySelector(".tag.active");
+
+    if (active) {
+      active.classList.remove("active");
+    }
+
+    event.target.parentNode.classList.add("active");
+    inView = event.target.innerText;
   }
+
+  let cur = 0;
+  let photos;
 
   if (timerID) {
     window.clearInterval(timerID);
   }
 
-  let photoCount = db.loved.length;
-  let photoSequence = 0;
-  //let photoUID = requestUID();
-
-  //clearGallery();
-  updateCurIndex(e);
-
-  if (!photoCount) {
-    return;
+  if (event.target.innerText === "Loved") {
+    photos = db.loved;
+  } else {
+    photos = db.getCollectionItems(event.target.innerText);
   }
 
-  gallery.placeholder(db.loved);
+  if (photos.length) {
+    gallery.placeholder(photos);
 
-  timerID = window.setInterval(() => {
-    if (photoCount - 1 == photoSequence) {
-      window.clearInterval(timerID);
-      timerID = null;
-    }
-    //loadSinglePhoto(db.loved[photoSequence], photoSequence, photoUID);
-    pexels.fetchSingle(db.loved[photoSequence]);
-    photoSequence++;
-  }, 1000);
+    timerID = window.setInterval(() => {
+      if (photos.length - 1 == cur) {
+        window.clearInterval(timerID);
+        timerID = null;
+      }
 
-  //loadPlaceholder(photoCount, photoUID);
-}
-
-//COLLECTIONS
-function loadCollectionPhotos(e) {
-  let index = e.target.parentNode.dataset.index;
-
-  if (index === curIndex) {
-    return;
+      pexels.fetchSingle(photos[cur]);
+      cur++;
+    }, 1000);
+  } else {
+    gallery.clear();
   }
-
-  if (timerID) {
-    window.clearInterval(timerID);
-  }
-
-  let photoSequence = 0;
-  let photoCollection = e.target.innerText;
-  let photos = db.getCollectionItems(photoCollection);
-  //let photoUID = requestUID();
-
-  //clearGallery();
-  updateCurIndex(e);
-
-  if (!photos.length) {
-    return;
-  }
-
-  gallery.placeholder(photos);
-
-  let photoCount = photos.length;
-
-  timerID = window.setInterval(() => {
-    if (photoCount - 1 == photoSequence) {
-      window.clearInterval(timerID);
-      timerID = null;
-    }
-    //loadSinglePhoto(photos[photoSequence], photoSequence, photoUID);
-    pexels.fetchSingle(photos[photoSequence]);
-    photoSequence++;
-  }, 1000);
-
-  //loadPlaceholder(photoCount, photoUID);
 }
 
 //OPEN REMOVE COLLECTION PROMPT
 function openPrompt(e) {
-  const index = e.target.parentNode.dataset.index - 1;
-  const name = db.collections[index].name;
+  const name = e.target.previousElementSibling.innerText;
   const message = "All of its content will be removed. This action cannot be undone.";
 
   const promptAction = document.querySelector(".remove-btn");
@@ -157,7 +107,7 @@ function openPrompt(e) {
   promptTitle.innerText = "Remove collection";
   promptMessage.innerText = `Remove "${name}" collection? ${message}`;
   popupPrompt.classList.add("popup-active");
-  promptAction.dataset.index = index;
+  promptAction.dataset.name = name;
   promptAction.addEventListener("click", removeCollection);
 }
 
@@ -177,20 +127,11 @@ function openRemovePrompt(e) {
 }
 
 function removePhoto(e) {
-  const name = db.collections[curIndex - 1].name;
-  console.log(name);
+  db.removeFromCollection(inView, photoID);
 
-  db.removeFromCollection(name, photoID);
+  const target = gallery.container.querySelector(`[data-uid="${photoID}"]`);
 
-  const cards = document.querySelectorAll(".card-action");
-  const gallery = document.querySelector(".gallery");
-
-  for (let i = 0; i < cards.length; i++) {
-    if (cards[i].dataset.id == photoID) {
-      gallery.removeChild(cards[i].parentNode.parentNode);
-      break;
-    }
-  }
+  gallery.container.removeChild(target);
 
   popupPrompt.classList.remove("popup-active");
   photoID = null;
@@ -198,18 +139,13 @@ function removePhoto(e) {
 
 //REMOVE COLLECTION AND ITS CONTENT
 function removeCollection(e) {
-  let index = Number(e.target.dataset.index);
-  const name = db.collections[index].name;
-  console.log(name);
+  const name = e.target.dataset.name;
 
   db.removeCollection(name);
 
-  //Update and load collections again
-  index++;
-
-  if (index == curIndex) {
-    clearGallery();
-    curIndex = null;
+  if (inView == name) {
+    gallery.clear();
+    inView = "";
   }
 
   loadCollections([{ name: "Loved" }, ...db.collections]);
